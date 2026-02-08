786379d1bd110edfcdfda77a0d4b1d5f9ffc0201
Rve27 <rve27github@gmail.com>
2026-02-08 10:48:30 +0700

    components: navigation: Handle Telegram link in AppBar
    Signed-off-by: Rve27 <rve27github@gmail.com>

--- composeApp/src/webMain/kotlin/com/rve/rvkernelmanager/ui/components/navigation/AppBar.kt
+++ composeApp/src/webMain/kotlin/com/rve/rvkernelmanager/ui/components/navigation/AppBar.kt
@@ -4,7 +4,6 @@ package com.rve.rvkernelmanager.ui.components.navigation
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
@@ -23,6 +22,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import com.composables.icons.materialsymbols.MaterialSymbols
@@ -31,6 +31,8 @@ import com.composables.icons.materialsymbols.roundedfilled.Menu
 object AppBar {
     @Composable
     fun SimpleTopAppBar() {
+        val uriHandler = LocalUriHandler.current
+
         var expanded by remember { mutableStateOf(false) }
 
         TopAppBar(
@@ -72,8 +74,21 @@ object AppBar {
                 shadowElevation = 16.dp
             ) {
                 DropdownMenuItem(
-                    text = { Text("Telegram") },
-                    onClick = { expanded = false }
+                    text = {
+                        Box(
+                            modifier = Modifier.fillMaxWidth(),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            Text(
+                                text = "Telegram",
+                                color = MaterialTheme.colorScheme.onSurface
+                            )
+                        }
+                    },
+                    onClick = {
+                        expanded = false
+                        uriHandler.openUri("https://t.me/rve_enterprises")
+                    }
                 )
             }
         }
